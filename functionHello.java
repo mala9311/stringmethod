fd19c97cde3620a71f5b9fb9d792f73c0bc8251e
mala9311 <165881518+mala9311@users.noreply.github.com>
2025-09-05 16:17:01 +0530

    Add files via upload

---
+++ functionHello.java
@@ -0,0 +1,9 @@
+class functionHello{
+    static void sayhello(String n){
+        String name = n;
+        System.out.println("hello " + name);
+    }
+    public static void main(String[] args){
+        sayhello("Archana");
+    }
+}
