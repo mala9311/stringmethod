a5368fd283b8d7ea1f3098df395310ea8a5462f5
mala9311 <malakumari9311@gmail.com>
2025-09-05 19:15:16 +0530

    Added Person class with input() and output() methods, and tested in Main class

--- stringmethod.java/Main.java
+++ stringmethod.java/Main.java
@@ -1,12 +1,32 @@
-public class Main { String name;
+class person{
+    String Name;
     int age;
+    void input(String n, int a){
+    Name = n;
+    age =a;
+}
+void output(){
+    System.out.println("Name =" + Name);
+    System.out.println("Age =" + age);
+}
+
+}
+
+public class Main { 
     public static void main(String[] args) {
-        Main obj = new Main();
-        obj.name ="Mala";
-        obj.age =21;
-        System.out.println("Name =" +obj.name);
-        System.out.println("age =" + obj.age);
+        person person1 = new person();
+        person1.input("mala",21);
+        person1.output();
     }
+    //String name;
+    // int age;
+    // public static void main(String[] args) {
+    //     Main obj = new Main();
+    //     obj.name ="Mala";
+    //     obj.age =21;
+    //     System.out.println("Name =" +obj.name);
+    //     System.out.println("age =" + obj.age);
+    // }
 
     
 }
