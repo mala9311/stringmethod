c856697b42c2f89f30118114aee4289c06e4d8b9
mala9311 <malakumari9311@gmail.com>
2025-09-05 18:52:53 +0530

    creating class object

---
+++ stringmethod.java/Main.java
@@ -0,0 +1,12 @@
+public class Main { String name;
+    int age;
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.name ="Mala";
+        obj.age =21;
+        System.out.println("Name =" +obj.name);
+        System.out.println("age =" + obj.age);
+    }
+
+    
+}
