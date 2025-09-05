d5b76681537103a8b94ea47b35dc2b96cd287b02
mala9311 <malakumari9311@gmail.com>
2025-09-05 16:47:55 +0530

    varible storing in function

---
+++ stringmethod.java/AddTwono.java
@@ -0,0 +1,10 @@
+public class AddTwono {
+    static int sum(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int sum = sum(5,5);
+        System.out.println(sum);
+    }
+    
+}
